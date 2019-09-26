2b40a6e64ded0c0c1eebe1e4ac3161c342f93c8b
unknown <gabimarim06@gmail.com>
2019-09-25 21:42:07 -0300

    ainda sem comunicação entre controllers

--- Controller/ControllerFXML.java
+++ Controller/ControllerFXML.java
@@ -1,10 +1,18 @@
-package Controller;
+package View;
 
 import Model.ResizableCanvas;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,6 +21,9 @@ public class ControllerFXML implements Initializable {
     @FXML
     private ResizableCanvas canvas1;
     GraphicsContext gc1;
+
+    @FXML
+    private MenuItem novo;
 
     //Método que faz tds as inicializações necessárias no início da execução do aplicativo
     @Override
@@ -25,4 +36,19 @@ public class ControllerFXML implements Initializable {
     public void drawall(){
 
     }
+
+    public void abreNew() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("new.fxml"));
+        Parent rootl = (Parent) loader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Gerar nova malha");
+        stage.setScene(new Scene(rootl));
+        stage.show();
+    }
+
+    public void novaMalha(int pontosX, int pontosZ, int espaco){
+
+    }
+
+
 }
---
+++ Controller/ControllerNewFXML.java
@@ -0,0 +1,31 @@
+package View;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerNewFXML implements Initializable {
+
+    @FXML
+    private TextField pontosX;
+
+    @FXML
+    private TextField pontosZ;
+
+    @FXML
+    private TextField espacoPontos;
+
+    @FXML
+    private Button gerar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+
+}
