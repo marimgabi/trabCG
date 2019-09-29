36f6e5ff1acaddf56f0ec9ec0f0a3d8785c73a35
unknown <gabimarim06@gmail.com>
2019-09-29 02:42:20 -0300

    Model Malha adicionado

--- Controller/ControllerMalha.java
+++ Controller/ControllerMalha.java
@@ -1,4 +1,25 @@
 package Controller;
 
+import Model.Malha;
+import Model.Vertice;
+
+import java.util.Random;
+
 public class ControllerMalha {
+    Malha malha;
+    Malha malhaVisualizacao;
+
+    public void criaMalha(int pontosX, int pontosZ, int espaco){
+        Random gerador = new Random(System.currentTimeMillis());
+
+        //Cria tds os vértices
+        for(int i=0;i<pontosX;i+=espaco){
+            for(int j=0;j<pontosZ;j+=espaco){
+                Vertice aux = new Vertice(i,Math.abs(gerador.nextDouble()),j);
+                //inserir os vertices
+
+            }
+        }
+    }
+
 }
